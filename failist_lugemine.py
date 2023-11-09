40d29206b58a71e982ac0b9990bbc1a5bc4f899f
irispajus <124361387+irispajus@users.noreply.github.com>
2023-11-09 09:52:25 +0200

    Update failist_lugemine.py

--- failist_lugemine.py
+++ failist_lugemine.py
@@ -4,9 +4,11 @@ exceli_asukoht_PCs = 'C:/Users/irispajus/Desktop/PY/loend.xlsx'
 
 workbook = openpyxl.load_workbook(exceli_asukoht_PCs)
 worksheet = workbook.active
+tulpade_arv = worksheet.max_column
 
-lahtri_vaartus = worksheet['A1'].value
-print(lahtri_vaartus)
+for i in range(1, tulpade_arv + 1):
+  lahter = worksheet.cell(row = 1, column = i)
+  print(lahter.value)
 
 
 
