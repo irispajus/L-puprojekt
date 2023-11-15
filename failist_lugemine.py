8616b45e29ff0bdeb8bcc8b167cdef07a5e95412
irispajus <124361387+irispajus@users.noreply.github.com>
2023-11-15 19:07:38 +0200

    Update failist_lugemine.py
    Kood saab hakkama Exceli failiga, mille vorming on:
    Tulp A- kõik käesolev info laulu kohta
    Tulp B- pealkirjaga "kaalupunktid" kuhu kaalupunkte lisatakse
    
    Testimiseks: sisesta ükskõik mis sõna, nt "pop" ja programm leiab iga laulu infost sama väärtuse

--- failist_lugemine.py
+++ failist_lugemine.py
@@ -1,14 +1,29 @@
 import openpyxl
 
-exceli_asukoht_PCs = 'C:/Users/irispajus/Desktop/PY/loend.xlsx'
+exceli_asukoht_PCs = 'siia lisa oma faili nimi, asukoht arvutis'
 
 workbook = openpyxl.load_workbook(exceli_asukoht_PCs)
 worksheet = workbook.active
-tulpade_arv = worksheet.max_column
 
-for i in range(1, tulpade_arv + 1):
-  lahter = worksheet.cell(row = 1, column = i)
-  print(lahter.value)
+#küsib lihtsalt märksõna mida failist otsida, võib olla nii zanr, osa pealkirjast kui ka artist
+kasutaja_sisestatud_laulunimi = input("Sisesta laulu nimi ")
+
+#otsib üles tabelist lahtri kaalupunktidega
+kaalupunktide_tulp = 'kaalupunktid' in [cell.value for cell in worksheet[1]]
+
+if kaalupunktide_tulp:
+    kaalupunktide_tulp_otsing = [cell.value for cell in worksheet[1]].index('kaalupunktid') + 1
+
+#käib kõik read läbi, kasutab ka funktsiooni lowercase, et kaotada suur- ja väiketähtede tundlikkus ära
+for row_index, row in enumerate(worksheet.iter_rows(min_row=2, max_row=worksheet.max_row), start=2):
+    for cell in row:
+        if kasutaja_sisestatud_laulunimi.lower() in str(cell.value).lower():
+            # liidab kaalupunktide lahtrisse väärtuse +1, vahet pole kas seal juba on juba üks väärtus või mitte
+            kaalupunktid = row[kaalupunktide_tulp_otsing - 1].value or 0
+            worksheet.cell(row=row_index, column= kaalupunktide_tulp_otsing, value= kaalupunktid + 1)
+              
+#salvestab tehtud muudatused
+workbook.save('siia lisa oma faili nimi, asukoht arvutis')
 
 
 
