b5602711bcbc8d55cf97af4f977256da8b9b3f3a
irispajus <124361387+irispajus@users.noreply.github.com>
2024-01-09 22:17:35 +0200

    Add files via upload

---
+++ start.py
@@ -0,0 +1,5 @@
+import subprocess
+
+subprocess.run(['python', 'C:/Users/irispajus/Desktop/PY/kaalupunktide_panemine.py'])
+
+subprocess.run(['python', 'C:/Users/irispajus/Desktop/PY/bubble_sort_algoritm.py'])
